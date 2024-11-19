0d17022cdad576dc568ff975167310ba8da23b55
Samuel Rocha <tubersamuel86@gmail.com>
2024-11-19 16:46:47 -0300

    exercicio1.js resolvido

---
+++ lista3/lib/exercicio1.js
@@ -0,0 +1,14 @@
+function avaliarmedia(media) {
+    if (media >= 7.0){
+        return "Aprovado"
+}
+
+        return "Reprovado"
+}
+
+console.log(avaliarmedia(10));
+console.log(avaliarmedia(7.0));
+console.log(avaliarmedia(6.95));
+console.log(avaliarmedia(8.8));
+console.log(avaliarmedia(0));
+console.log(avaliarmedia(2.725));
