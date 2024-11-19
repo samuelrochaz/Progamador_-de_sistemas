d382d3f9730743602dc641c5f77bc1e89dd282f0
Samuel Rocha <tubersamuel86@gmail.com>
2024-11-19 17:24:20 -0300

    exercicio2.js

---
+++ lista3/lib/exercicio2.js
@@ -0,0 +1,10 @@
+function Impar0upar(nm) {
+    if(nm % 2 ==0){
+        return "Par"
+}
+return "Impar"
+}
+console.log(Impar0upar(4));
+console.log(Impar0upar(7));
+console.log(Impar0upar(0));
+console.log(Impar0upar(-5));
