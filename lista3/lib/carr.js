b1cba94611a99428ed6e75705b883cbbe243383d
Samuel <tubersamuel86@gmail.com>
2024-11-26 17:23:16 -0300

    carr.js

---
+++ lista3/lib/carr.js
@@ -0,0 +1,17 @@
+var carro = {
+
+    marca:"Toyota",
+    modelo: "Supramk4",
+    ano:2010,
+    capacidade_de_peso:"160kg",
+    veloxidade_max:"260km/h",
+    motor:"V10",
+combustão:"Dissel",
+    cambio:"manual",
+    fab:"Tokyo",
+    calibragem_do_pneu:"32psi"
+
+}
+
+console.log(carro);
+console.table(carro);
