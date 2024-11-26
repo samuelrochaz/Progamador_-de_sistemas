5cd07bc2e7f447384e2ea6c1c321f16bc2e73f80
Samuel <tubersamuel86@gmail.com>
2024-11-26 16:19:36 -0300

    exercicio.js

--- lista3/lib/exercicio4.js
+++ lista3/lib/exercicio4.js
@@ -1,12 +1,9 @@
 function Menor (n1, n2, n3) {
     let menor = n1;
-    //comparar o primeiro numero com o segundo
-    if (n2 < menor) {
-        menor = n2;
-    }
-    if (n3 < menor) {
-        menor = n3;
-    }
+
+    if (n2 < menor) menor = n2;
+    if (n3 < menor) menor = n3;
+    
     return menor;
 }
 console.log(Menor(3, 7, 5));
