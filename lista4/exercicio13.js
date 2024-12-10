bbea3b597af1bddcd5499355ab5349cc9cb7099c
Samuel <tubersamuel86@gmail.com>
2024-12-10 17:24:05 -0300

    exercicio13.js

--- lista4/exercicio13.js
+++ lista4/exercicio13.js
@@ -1,4 +1,10 @@
 function Maior(lista) {
+    var resu = Math.max(...lista)
 
+    return resu
 }
-// console.log(Maior([1, 2, 3, 4, 5]));
+
+console.log(Maior([1, 2, 3, 4, 5]));
+console.log(Maior([-1, -2, -3, -4, -5]));
+console.log(Maior([10, 20, 30, 40, -50]));
+console.log(Maior([]));
