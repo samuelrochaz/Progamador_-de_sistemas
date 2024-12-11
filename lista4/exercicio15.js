e9f3d0fa282af02e3b6f08aa3070f661c8c2b090
Samuel <tubersamuel86@gmail.com>
2024-12-11 17:14:00 -0300

    exercicio15.js

---
+++ lista4/exercicio15.js
@@ -0,0 +1,7 @@
+function reverso(xx) {
+    var aa = xx.split('').reverse().join('');
+    return aa 
+}
+console.log(reverso("hellow"));
+console.log(reverso("world"));
+console.log(reverso("abc"));
