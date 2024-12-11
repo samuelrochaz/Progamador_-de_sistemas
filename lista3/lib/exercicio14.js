92b3babb52d27d8131887e603c85d423b3bf7813
Samuel <tubersamuel86@gmail.com>
2024-12-11 17:13:35 -0300

    exercicio14.js

--- lista3/lib/exercicio14.js
+++ lista3/lib/exercicio14.js
@@ -3,18 +3,19 @@ function encontrarPrimo(np) {
     for (var div = 2; div < np; div++) {
         if (np % div === 0) {
 
-            return "false"
+            return false
         }
-        return "true"
+        return true
     }
 }
+const numeros = [7, 10, 13, 0, 1, 2, 327, 847, 1573, 31, -3, -31]
+const primos = numeros.filter(encontrarPrimo);
 console.log(encontrarPrimo(7));
 console.log(encontrarPrimo(10));
 console.log(encontrarPrimo(13));
 console.log(encontrarPrimo(0));
 console.log(encontrarPrimo(1));
 console.log(encontrarPrimo(2));
-console.log("========")
 console.log(encontrarPrimo(327));
 console.log(encontrarPrimo(847));
 console.log(encontrarPrimo(1573));
