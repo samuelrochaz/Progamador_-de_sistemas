74142f9a9e12550327973710698b650b24d4fc4e
Samuel <tubersamuel86@gmail.com>
2024-12-13 17:27:44 -0300

    lista4

--- lista4/exercicio11.js
+++ lista4/exercicio11.js
@@ -1,22 +1,3 @@
-// function calcularmedia(md) {
-//     var media = 0;
-
-//     for (var i = 0; i < md.length; i++) {
-//         media += md[i] / md.length;
-// }
-// return media
-// }
-// var lista1 = [1, 2, 4, 5, 7, 8, 10, 11]
-// console.log(calcularmedia(lista1));
-
-// var lista2 = [10, 70, 30, 50]
-// console.log(calcularmedia(lista2));
-
-// var lista3 = [29, 567, 38, 53, 503, 92, 67, 909, 203, 382, 28]
-// console.log(calcularmedia(lista3));
-
-// var lista4 = []
-// console.log(calcularmedia(lista4));
 const lista = [1, 2, 3, 4, 5]
 function soma(a, b){
     return a + b /lista.length;
--- lista4/exercicio18.js
+++ lista4/exercicio18.js
@@ -1,12 +1,13 @@
-const vogais = ["a", "e", "i", "o", "u"]
 function contarVogais(xx) {
+    const vogais = ["a", "e", "i", "o", "u"]
     const sep = xx.split('')
-    const aa = vogais.includes(xx)
-    console.log(aa)
-    // const letras = aa.filter(contarVogais) 
+    const vogl = sep.filter(letras => vogais.includes(letras))
+
+return vogl.length;
+// const letras = aa.filter(contarVogais) 
 // separar todas as letras uma das outras 
 // transformar em array
 //comparar as letras com as vogais
 // filtrar as vogais que sao iguais as da lista e contar as vogais
 }
- console.log(contarVogais("javascript"))
+ console.log(contarVogais("javascript" ) +" vogais")
