e765b69872ece36396b24a3aaeff59ba11ee04aa
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:10:41 -0300

    biblioteca.js

--- Exercicios/java script/final/biblioteca.js
+++ Exercicios/java script/final/biblioteca.js
@@ -51,9 +51,114 @@ export function imparOupar(nm) {
 // lista4 - exercicio12
 export function somarDigito(numero) {
     var digito = numero.toString().split('')
-    var soma = somaLista(digito)
-    // var soma = 0;
-    // for (var i = 0; i < list.length; i++) {
-    //     soma += Number(aa[i]);
+    var soma = 0;
+    for (var i = 0; i < digito.length; i++) soma += Number(digito[i]);
      return soma
+}
+// lista4 - exerciciio11
+export function Mediaarray(num){
+    var media = num.reduce((a, b, Index, lista) => a + b  / lista.length);
+    return media.toFixed(1)
+}
+
+//lista4 exercicio15
+export function reverso(xx) {
+    var aa = xx.split('').reverse().join('');
+    return aa 
+}
+
+//lista4 - exercicio17
+export function verificarString (xx) {
+    return !isNaN(xx)
+}
+
+//lista4 - exercicio18
+export function contarVogais(xx) {
+    const vogais = ["a", "e", "i", "o", "u"]
+    const sep = xx.split('')
+    const vogl = sep.filter(letras => vogais.includes(letras))
+    
+    return vogl.length;
+    
+}
+
+//lista3 - exercicio1
+export function avaliarmedia(media) {
+    if (media >= 7.0){
+        return "Aprovado"
+    }
+    
+    return "Reprovado"
+}
+
+//lista3 - exercicio3
+export function neg_pos(n_ou_p) {
+    if (n_ou_p > 0){
+        return "Positivo"
+    }
+    if (n_ou_p == 0){
+        return "Zero"
+    }
+    return "Negativo"
+}
+
+//lista3 - exercicio4
+export function Menor (n1, n2, n3) {
+    let menor = n1;
+    
+    if (n2 < menor) menor = n2;
+    if (n3 < menor) menor = n3;
+    
+    return menor;
+}
+
+
+//lista3 - exercicio5
+export function Maiordetds (n1, n2, n3) {
+    let lista = [n1, n2, n3]
+    let num = Maior(lista);
+    return num
+}
+
+//lista3 - exercicio6
+export function multiplo (n1, n2) {
+    
+    return n2 !== 0 && n1 % n2 === 0;
+}
+
+//lista3 - exercicio7
+export function verificarBissexto(ano) {
+    if (ano % 400 === 0) return true;
+    
+    if (ano % 100 === 0) return false;
+    
+    if (ano % 4 === 0) return true;
+    
+    return "false"
+}
+
+//lista3 - exercicio8
+export function verificarletra(letra) {
+    if ('aeiou'.includes(letra)) {
+        return "Vogal";
+    } else {
+        return "Consoante";
+    }
+}
+
+//lista3 - exercicio14
+export function tabuada(n){
+    for(var k=0; k <= 10; k++) console.log(`${n} x ${k} = ${n*k}`);
+    }
+
+//lista3 - exercicio14
+export function encontrarPrimo(np) {
+    if (np < 2) return "false";
+    for (var div = 2; div < np; div++) {
+        if (np % div === 0) {
+
+            return false
+        }
+        return true
+    }
 }
