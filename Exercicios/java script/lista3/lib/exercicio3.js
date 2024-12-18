b21da0b62d13e078b228f424f5857442c3bbed6e
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:07:49 -0300

    exercicico3.js

--- Exercicios/java script/lista3/lib/exercicio3.js
+++ Exercicios/java script/lista3/lib/exercicio3.js
@@ -1,14 +1,6 @@
-function verifique(n_ou_p) {
-if (n_ou_p > 0){
-    return "Positivo"
-}
-if (n_ou_p == 0){
-    return "Zero"
-}
- return "Negativo"
-}
-console.log(verifique(5));
-console.log(verifique(-3));
-console.log(verifique(0));
-console.log(verifique(1110));
-console.log(verifique(-987654321));
+import { neg_pos } from "../../final/biblioteca.js";
+console.log(neg_pos(5));
+console.log(neg_pos(-3));
+console.log(neg_pos(0));
+console.log(neg_pos(1110));
+console.log(neg_pos(-987654321));
