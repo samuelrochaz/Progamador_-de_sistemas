09ef1ee0edfcd866fd37de714fd5d592c2b5de96
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:10:01 -0300

    exercicio18.js

--- Exercicios/java script/lista4/exercicio18.js
+++ Exercicios/java script/lista4/exercicio18.js
@@ -1,13 +1,3 @@
-function contarVogais(xx) {
-    const vogais = ["a", "e", "i", "o", "u"]
-    const sep = xx.split('')
-    const vogl = sep.filter(letras => vogais.includes(letras))
+import { contarVogais } from "../final/biblioteca.js";
 
-return vogl.length;
-// const letras = aa.filter(contarVogais) 
-// separar todas as letras uma das outras 
-// transformar em array
-//comparar as letras com as vogais
-// filtrar as vogais que sao iguais as da lista e contar as vogais
-}
  console.log(contarVogais("javascript" ) +" vogais")
