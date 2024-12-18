cdcfd2cfdc2dddda0b72ecf178d5302756884294
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:08:00 -0300

    exercicico4.js

--- Exercicios/java script/lista3/lib/exercicio4.js
+++ Exercicios/java script/lista3/lib/exercicio4.js
@@ -1,11 +1,5 @@
-function Menor (n1, n2, n3) {
-    let menor = n1;
+import { Menor } from "../../final/biblioteca.js";    
 
-    if (n2 < menor) menor = n2;
-    if (n3 < menor) menor = n3;
-    
-    return menor;
-}
 console.log(Menor(3, 7, 5));
 console.log(Menor(-1, -5, -3));
 console.log(Menor(8, 2, 10));
