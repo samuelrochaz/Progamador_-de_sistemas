36bb78221996c43fed6e987be76771ec3b2e3135
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:09:13 -0300

    exercicico11.js

--- Exercicios/java script/lista4/exercicio11.js
+++ Exercicios/java script/lista4/exercicio11.js
@@ -1,6 +1,5 @@
-const lista = [1, 2, 3, 4, 5]
-function soma(a, b){
-    return a + b /lista.length;
-}
-const total = lista.reduce(soma, 0);
-console.log(total)
+import { Mediaarray } from "../final/biblioteca.js";
+
+console.log(Mediaarray([1, 2, 3, 4, 5, 7, 8, 10, 11]))
+console.log(Mediaarray([10, 70, 30, 50]))
+console.log(Mediaarray([29, 567, 38, 53, 503, 92, 67, 909, 203, 382, 28]))
