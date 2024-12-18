f4a98527a710e4ecfad04a367ca6e9fe7ec32c63
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:11:04 -0300

    teste.js

--- Exercicios/java script/lista4/testemap.js
+++
@@ -1,8 +0,0 @@
-const str = "3 7 9 10 13";
-const lines = str.split(' ').map(Number);
-
-function soma(a, b) {
-    return a + b / lines.length;
-}
-const total = lines.reduce(soma, 0);
-console.log(total)
