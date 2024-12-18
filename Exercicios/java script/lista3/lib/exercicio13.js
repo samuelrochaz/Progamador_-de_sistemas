d29540878da1135ee2cff2ee420d8032d6b38ac7
Samuel Alves <tubersamuel86@gmail.com>
2024-12-18 17:08:56 -0300

    exercicico13.js

--- Exercicios/java script/lista3/lib/exercicio13.js
+++ Exercicios/java script/lista3/lib/exercicio13.js
@@ -1,5 +1,3 @@
-function tabuada(n){
-for(var k=0; k <= 10; k++) console.log(`${n} x ${k} = ${n*k}`);
-}
+import { tabuada } from "../../final/biblioteca.js";
 
 for(var n=1; n <= 10; tabuada(n++));
