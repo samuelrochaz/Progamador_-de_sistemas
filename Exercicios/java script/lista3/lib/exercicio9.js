4f90325143aefd889a29c6bfb3b0c30b5de9cb7b
samuel <tubersamuel86@gmail.com>
2024-12-20 11:15:59 -0300

    exercicio9.js

---
+++ Exercicios/java script/lista3/lib/exercicio9.js
@@ -0,0 +1,11 @@
+import { notas } from "../../final/biblioteca.js"
+
+console.log(notas(90))
+console.log(notas(75))
+console.log(notas(60))
+console.log(notas(0))
+console.log(notas(59))
+console.log(notas(89))
+console.log(notas(69))
+console.log(notas(79))
+console.log(notas(99))
